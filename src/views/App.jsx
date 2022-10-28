df8d4e43eb8ea51fde426d405891158207a40ee5
v1n1c1us5ouza <viniciusdesouzamarinho.com>
2022-10-28 11:26:17 -0300

    Learned how to use useContext

--- src/views/App.jsx
+++ src/views/App.jsx
@@ -5,15 +5,21 @@ import { BrowserRouter as Router } from  'react-router-dom'
 import Menu from '../components/layout/Menu'
 import Content from '../components/layout/Content'
 
+import DataContext, { data } from '../data/DataContext'
+import { useState } from 'react'
+
 const App = props => {
+    const [state, setState] = useState(data)
 
     return (
-                <div className="App">
-                    <Router>
-                        <Menu />
-                        <Content />
-                    </Router>
-                </div>
+        <DataContext.Provider value={{state, setState}}>
+          <div className="App">
+                <Router>
+                    <Menu />
+                    <Content />
+                </Router>
+            </div>
+        </DataContext.Provider>
     )
 }
 

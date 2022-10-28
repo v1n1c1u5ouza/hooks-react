079b505b58413220805b87794932da8f091fa0c6
v1n1c1us5ouza <viniciusdesouzamarinho.com>
2022-10-28 13:00:25 -0300

    Update

--- src/views/examples/UseReducer.jsx
+++ src/views/examples/UseReducer.jsx
@@ -2,32 +2,7 @@ import React, { useReducer } from 'react'
 import { StaticRouter } from 'react-router-dom'
 import PageTitle from '../../components/layout/PageTitle'
 
-const initialState = {
-    cart: [],
-    products: [],
-    user: null,
-    number: 0
-}
-
-function reducer(state, action) {
-    switch(action.type) {
-        case 'numberAdd2':
-            return { ...state, number: state.number + 2 }
-            case 'numberMulti7':
-            return { ...state, number: state.number * 7 }
-            case 'numberDiv25':
-            return { ...state, number: state.number / 25 }
-            case 'numberInt':
-            return { ...state, number: parseInt(state.number)}
-            case 'numberAddN':
-                return { ...state, number: state.number + action.payload }
-        case 'login':
-            return { ...state, user: { name: action.payload } }
-        default: 
-            return state
-    }
-
-}
+import { initialState, reducer } from '../../store'
 
 const UseReducer = (props) => {
     const [state, dispatch] = useReducer(reducer, initialState)
